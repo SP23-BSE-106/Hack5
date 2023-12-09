3d4c030d188e5ba036de65b44a89952487116bc2
Munazza Javed <munazzajaved277@gmail.com>
2023-12-09 22:23:06 +0500

    utlis.h added

--- utlis.c
+++ utlis.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
+#include "utlis.h"
 
 int main(){
     double originLatitude , originLongitude , destinationLatitude , destinationLongitude;
---
+++ utlis.h
@@ -0,0 +1,5 @@
+//prototypes
+
+double degressToRadians(double degree);
+double getAirDistance(double originLatitude , double originLongitute ,double destinationLatitude , double destinationLongitude );
+double lorentzTimeDilation(double t, double percent);
